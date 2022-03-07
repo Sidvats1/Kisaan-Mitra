278f49d3b69691e1180346f1b2d8991d6392965d
Siddharth Mishra <69946081+Sidvats1@users.noreply.github.com>
2022-03-07 20:35:48 +0530

    first commit

---
+++ lib/FadeAnimation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:simple_animations/simple_animations.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeAnimation(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
